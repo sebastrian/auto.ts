6ce5717591f0888957f51bc337ba2a75d40aaa86
sebastrian <103613105+sebastrian@users.noreply.github.com>
2022-10-01 00:12:24 +0200

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,6 @@
+let readlineSync = require("readline-sync");
+let primera = readlineSync.questionInt("primera e 1 cuadra");
+let segunda = readlineSync.questionInt("segunda en 2 cuadra");
+let tercera = readlineSync.questionInt("tercera en 3 cuadra");
+
+let cuadraTotal = primera + segunda + tercera;
